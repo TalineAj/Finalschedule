92bb920c2023c37b536182122d5c017dbdb77e44
Taline Abou Jaoude <taline.aboujaoude@lau.edu>
2022-05-05 15:15:02 +0300

    designing and linking layout

--- app/src/main/java/com/lau/finalschedule/MainActivity.java
+++ app/src/main/java/com/lau/finalschedule/MainActivity.java
@@ -2,13 +2,33 @@ package com.lau.finalschedule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+        ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.listview); //linking the list view
+        ArrayList <String> courses = new ArrayList<>(), links = new ArrayList<>();
+
+
+        //Changing color of status bar in android app
+        // Source://https://www.geeksforgeeks.org/how-to-change-the-color-of-status-bar-in-an-android-app/
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.teal_200));
+        }
+
+
     }
 }
