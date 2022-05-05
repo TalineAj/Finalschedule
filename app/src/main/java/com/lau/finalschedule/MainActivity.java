b2b497e6182ad4efe3a7b177c8322c408b7f9448
Taline Abou Jaoude <taline.aboujaoude@lau.edu>
2022-05-05 18:07:23 +0300

    final

--- app/src/main/java/com/lau/finalschedule/MainActivity.java
+++ app/src/main/java/com/lau/finalschedule/MainActivity.java
@@ -2,34 +2,61 @@ package com.lau.finalschedule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
         ListView list;
+        Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         list = (ListView) findViewById(R.id.listview); //linking the list view
-        ArrayList <String> courses = new ArrayList<>(), links = new ArrayList<>();
+        ArrayList <String> courses = new ArrayList<>();
+        ArrayList <String> links = new ArrayList<>();
+        
 
+        try {
+            SQLiteDatabase sql = this.openOrCreateDatabase("coursesdb", MODE_PRIVATE, null);
+//             sql.execSQL("CREATE Table IF NOT EXISTS courses (course_name VARCHAR, link VARCHAR)");
+//             sql.execSQL("INSERT INTO courses(course_name, link) VALUES ('Mobile Computing', 'https://ionicframework.com/'), ('Software Engineering', 'https://www.edx.org/learn/software-engineering'),('Parallel programming', 'https://www.coursera.org/learn/scala-parallel-programming'), ('Discrete Mathematics', 'https://www.codecademy.com/learn/discrete-math')");
 
-        //Changing color of status bar in android app
-        // Source://https://www.geeksforgeeks.org/how-to-change-the-color-of-status-bar-in-an-android-app/
-        if (Build.VERSION.SDK_INT >= 21) {
-            Window window = this.getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(this.getResources().getColor(R.color.teal_200));
+            Cursor c = sql.rawQuery("SELECT * FROM courses", null);
+            int course_name_index = c.getColumnIndex("course_name");
+            int link_index = c.getColumnIndex("link");
+
+            while (c.moveToNext()) {
+                courses.add(c.getString(course_name_index));
+                links.add(c.getString(link_index));
+
+            }
+            ArrayAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, courses);
+            list.setAdapter(adapter);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
 
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long x) {
+                intent = new Intent(getApplicationContext(), MainActivity2.class);
+                intent.putExtra("studylink", links.get(i)); //sending data from this activity to the other, the link the user clicks on
+                startActivity(intent);
+            }
+        });
 
 
 
--- app/src/main/java/com/lau/finalschedule/MainActivity2.java
+++ app/src/main/java/com/lau/finalschedule/MainActivity2.java
@@ -2,6 +2,7 @@ package com.lau.finalschedule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,10 +15,13 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        Intent intent = getIntent(); //linking with the other activity
+        String link = intent.getStringExtra("studylink");
+
         webview = (WebView) findViewById(R.id.webview); //linking with view
         webview.getSettings().setJavaScriptEnabled(true); //enabaling js
         webview.setWebViewClient(new WebViewClient());//set client for this view
-        webview.loadUrl(); //Link is the link of the website
+        webview.loadUrl(link); //Link is the link of the website
 
 
 
